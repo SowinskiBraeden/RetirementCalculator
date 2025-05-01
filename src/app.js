3ef8129196ebd6dbd17ddbf0013845a6a9e9800c
Joaquin <jparedes.peake@gmail.com>
2025-04-30 17:15:23 -0700

    update/created routing for pages

--- src/app.js
+++ src/app.js
@@ -1,9 +1,72 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 8000;
 
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'views'));
+
+/*
+ROUTINGS
+*/
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/home', (req, res) => {
+    res.render('home');
+});
+
+app.get('/assets', (req, res) => {
+    res.render('assets');
+});
+
+app.get('/plans', (req, res) => {
+    res.render('plans');
+});
+
+app.get('/more', (req, res) => {
+    res.render('more');
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profiles');
+});
+
+app.get('/settings', (req, res) => {
+    res.render('settings');
+});
+
+app.get('/aboutUs', (req, res) => {
+    res.render('aboutUs');
+});
+
+
+app.post('/signup', (req, res) => {
+    res.status(200);
+    res.send('200 status code');
+});
+
+app.post('/login', (req, res) => {
+    res.status(200);
+    res.send('200 status code');
+});
+
+app.get('/*splat', (req, res) => {
+    res.status(404);
+    res.send('404 Not Found');
+});
+
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
